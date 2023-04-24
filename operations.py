56e4650fe76e008806098e1daf02a35d05430230
Andreas <andreas.michael163@gmail.com>
2023-04-24 22:57:29 +0200

    Add comments to operations script

--- operations.py
+++ operations.py
@@ -1,12 +1,15 @@
+# Input parameters
 a = 15
 b = 2
 
+# Addition 
 def add_nums(num1, num2):
     total = num1 + num2
     print(num1,"+",num2,"=",total)
 
 add_nums(a,b)
 
+# Subtraction
 def sub_nums(num1,num2):
     if num1 > num2:
         total = num1 - num2
@@ -17,6 +20,7 @@ def sub_nums(num1,num2):
 
 sub_nums(a,b)
 
+# Multiplication
 def mult_nums(num1,num2):
         total = num1 * num2
         print(num1,"*",num2,"=",total)
