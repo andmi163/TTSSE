19c67de7dac35482c91a106ffd0dae4d6b44a87d
Andreas <andreas.michael163@gmail.com>
2023-04-24 22:55:14 +0200

    Add subtration to operations

--- operations.py
+++ operations.py
@@ -1,5 +1,18 @@
+a = 15
+b = 2
+
 def add_nums(num1, num2):
     total = num1 + num2
     print(num1,"+",num2,"=",total)
 
-add_nums(1,2)
+add_nums(a,b)
+
+def sub_nums(num1,num2):
+    if num1 > num2:
+        total = num1 - num2
+        print(num1,"-",num2,"=",total)
+    else:
+        total = num2 - num1
+        print(num2,"-",num1,"=",total) 
+
+sub_nums(a,b)
